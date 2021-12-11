fc443592fbee72a1d5f507b571769802ff514d2e
tomas <72792707+tomas-land@users.noreply.github.com>
2021-12-11 19:55:19 +0200

    init

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,19 @@
+<x-guest-layout>
+
+    <div class=" flex justify-center bg-gray-100 dark:bg-gray-900 md:bg-black">
+        @if (Route::has('login'))
+            <div class="">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+    
+</x-guest-layout>
