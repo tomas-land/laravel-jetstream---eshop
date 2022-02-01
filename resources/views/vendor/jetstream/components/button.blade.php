c50fd1d604eaefcf7dbe0238fc1c8ba078ecce6a
tomas <72792707+tomas-land@users.noreply.github.com>
2022-02-01 15:20:37 +0200

    created two login forms in login page

--- resources/views/vendor/jetstream/components/button.blade.php
+++ resources/views/vendor/jetstream/components/button.blade.php
@@ -1,3 +1,5 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'uppercase inline-flex items-center px-12 py-4 bg-gray-800 border border-transparent font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'uppercase inline-flex items-center px-12 py-4 bg-gray-800 
+border border-transparent font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 
+focus:outline-none disabled:opacity-25 transition']) }}>
     {{ $slot }}
 </button>
